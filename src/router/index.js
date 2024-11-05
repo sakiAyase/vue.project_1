9155db70044b8dff5b223c984745b442e7fe422f
L4palce <1906518568@qq.com>
2024-11-05 11:14:01 +0800

    更新路由配置为history

--- src/router/index.js
+++ src/router/index.js
@@ -1,25 +1,22 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 
-
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    /* name: '',
-    component: ()=>import('../views/tlias/EmpView.vue') */
-    redirect:'/log'//这个就是值访问path为/时默认访问redirect里面的path！！！，其实和上面两行的效果是一样的
+    redirect: '/log' // 访问根路径时重定向到 /log
   },
   {
     path:'/log',
     name:'login',
-    component:()=>import('../views/auth/LoginView.vue')
+    component: ()=>import('../views/auth/LoginView.vue')
   },  
   {
     path:'/reg',
     name:'register',
-    component:()=>import('../views/auth/RegisterView.vue')
+    component: ()=>import('../views/auth/RegisterView.vue')
   },
   {
     path: '/emp',
@@ -29,22 +26,17 @@ const routes = [
   {
     path: '/dept',
     name: 'dept',
-    component: () => import(/* webpackChunkName: "about" */ '../views/tlias/deptView.vue')
+    component: () => import('../views/tlias/deptView.vue')
   },
   {
     path: '/ClC',
     name: 'ClC',
-    component: () => import(/* webpackChunkName: "about" */ '../views/tlias/ClassComponent.vue')
-  },
-  {
-    path: '/IMG',
-    name: 'img',
-    component: () => import(/* webpackChunkName: "about" */ '../views/tlias/ImageView.vue')
+    component: () => import('../views/tlias/ClassComponent.vue')
   }
-
 ]
 
 const router = new VueRouter({
+  mode: 'history', // 设置为 history 模式
   routes
 })
 
